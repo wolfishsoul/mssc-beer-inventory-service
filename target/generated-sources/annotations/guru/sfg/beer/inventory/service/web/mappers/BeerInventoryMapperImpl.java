e0368d3dfbcce33135ef32277d433a74a7f53286
Viktor Ivanov <viktor94@gmail.com>
2020-04-19 18:21:54 +0200

    Adding mysql jdbc config and init script

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-18T15:05:14+0200",
+    date = "2020-04-19T18:19:25+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
